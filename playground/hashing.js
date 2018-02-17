8d180428e9d8ca633cd1536d671be93e26ddb912
almufadhli <a.c.7@hotmail.com>
2018-02-17 20:17:35 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var salt = bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
+var hashedPass = '$2a$10$5h1a5PDjRL/66DXPEDKJwOupL3OAG/.kh1YFwIvSzm6RO.Q.qQo9G';
+
+bcrypt.compare(password, hashedPass, (err, res) => {
+  console.log(res);
+});
+
+//
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded:', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -79,6 +80,21 @@ UserSchema.statics.findByToken = function (token) {
 
 };
 
+// run this event before saving the docu to the database. This is mongoose middleware
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.hash(user.password, 8, function (err, hash) {
+      user.password = hash;
+      next();
+    });
+
+
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
